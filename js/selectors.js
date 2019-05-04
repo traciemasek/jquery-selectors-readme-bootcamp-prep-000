f3f32e1d747bd5da50bfffcda51b993650fa28d0
Tracie Masek <traciemasek@gmail.com>
2019-05-04 19:00:00 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,25 @@
 // declare your functions here...
-const paragraphSelector = () => {
+/*const paragraphSelector = () => {
+  return $('p');
+};
+
+const lastImageSelector = () => {
+  return $('img:last');
+};
+
+const ninjaBabySelector = () => {
+  return $('#baby-ninja');
+};
+
+const divSelector = () => {
+  return $('.pics');
+};
+
+const firstListItem = () => {
+  return $('ul #pic-list:first-child');
+}; */
+
+function paragraphSelector() {
   return $('p');
 };
 
